72465976c624b99abb32f09f196e2d83839dba1e
Nils <nils.quetschlich@gmail.com>
2021-11-16 16:37:33 +0100

    modified graph state that different qubit numbers are possible

--- src/benchmarks/graph_state.py
+++ src/benchmarks/graph_state.py
@@ -8,7 +8,7 @@ def create_circuit(n):
     c = ClassicalRegister(n, 'c')
     qc = QuantumCircuit(q, c, name="graph_state")
 
-    G = nx.random_regular_graph(3, n)
+    G = nx.random_regular_graph(4, n)
     A = nx.convert_matrix.to_numpy_array(G)
     qc.compose(GraphState(A), inplace=True)
     qc.measure_all()
