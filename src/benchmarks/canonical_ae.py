dcaad9c69c3866dfa804594043db783634cf1b78
Nils <nils.quetschlich@gmail.com>
2021-12-03 15:42:02 +0100

    adjusted AE benchmark

---
+++ src/benchmarks/canonical_ae.py
@@ -0,0 +1,17 @@
+## Code from https://qiskit.org/documentation/finance/tutorials/00_amplitude_estimation.html
+
+from qiskit.algorithms import AmplitudeEstimation
+
+from src import utils
+
+
+def create_circuit(num_qubits: int):
+    ae = AmplitudeEstimation(
+        num_eval_qubits=num_qubits - 1,  # magic number one because of to be estimated qubit
+    )
+    problem = utils.get_estimation_problem()
+
+    qc = ae.construct_circuit(problem)
+    qc.name = "canonical_ae"
+
+    return qc
