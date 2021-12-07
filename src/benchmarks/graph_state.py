27aeb8a15dfb559e2f6dc5ba3a151a9555e32104
Nils <nils.quetschlich@gmail.com>
2021-12-07 09:22:05 +0100

    bugfixed and checked graph state

--- src/benchmarks/graph_state.py
+++ src/benchmarks/graph_state.py
@@ -3,12 +3,13 @@ from qiskit.circuit.library import GraphState
 
 import networkx as nx
 
-def create_circuit(n):
-    q = QuantumRegister(n, 'q')
-    c = ClassicalRegister(n, 'c')
-    qc = QuantumCircuit(q, c, name="graph_state")
+#checked
 
-    G = nx.random_regular_graph(4, n)
+def create_circuit(num_qubits:int, degree:int = 2):
+    q = QuantumRegister(num_qubits, 'q')
+    qc = QuantumCircuit(q, name="graph_state")
+
+    G = nx.random_regular_graph(degree, num_qubits)
     A = nx.convert_matrix.to_numpy_array(G)
     qc.compose(GraphState(A), inplace=True)
     qc.measure_all()
