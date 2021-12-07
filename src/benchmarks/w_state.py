2344661b77e8e7a61ae75b694eddef7ab70e4ed0
Nils <nils.quetschlich@gmail.com>
2021-12-07 09:39:23 +0100

    fixed bug and checked w_state

--- src/benchmarks/w_state.py
+++ src/benchmarks/w_state.py
@@ -1,11 +1,11 @@
 import numpy as np
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
 
+## checked
 
 def create_circuit(n: int):
     q = QuantumRegister(n, 'q')
-    c = ClassicalRegister(n, 'c')
-    qc = QuantumCircuit(q, c, name="w_state")
+    qc = QuantumCircuit(q, name="w_state")
 
     def f_gate(qc: QuantumCircuit, q: QuantumRegister, i: int, j: int, n: int, k: int):
         theta = np.arccos(np.sqrt(1 / (n - k + 1)))
@@ -14,10 +14,13 @@ def create_circuit(n: int):
         qc.ry(theta, q[j])
 
     qc.x(q[-1])
+
     for l in range(1, n):
         f_gate(qc, q, n - l, n - l - 1, n, l)
+
     for l in reversed(range(1, n)):
         qc.cx(l - 1, l)
+
     qc.measure_all()
 
     return qc
