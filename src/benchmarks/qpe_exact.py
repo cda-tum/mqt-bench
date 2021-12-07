c4e5c397ad41d4ab1441d9de2ace43209eb0de0e
Nils <nils.quetschlich@gmail.com>
2021-12-07 11:40:06 +0100

    bugfixed and checked both qpe benchmarks

--- src/benchmarks/qpe_exact.py
+++ src/benchmarks/qpe_exact.py
@@ -10,7 +10,7 @@ def create_circuit(n: int):
     n = n-1 # because of ancilla qubit
     q = QuantumRegister(n, 'q')
     psi = QuantumRegister(1, 'psi')
-    c = ClassicalRegister(n + 1, 'c')
+    c = ClassicalRegister(n, 'c')
     qc = QuantumCircuit(q, psi, c, name="qpe_exact")
 
     # get random n-bit string as target phase
@@ -18,7 +18,7 @@ def create_circuit(n: int):
     while theta == 0:
         theta = random.getrandbits(n)
     lam = Fraction(0, 1)
-    # print("theta : ", theta, "correspond to", theta / (1 << n), "bin: ")
+    #print("theta : ", theta, "correspond to", theta / (1 << n), "bin: ")
     for i in range(n):
         if theta & (1 << (n - i - 1)):
             lam += Fraction(1, (1 << i))
@@ -34,6 +34,6 @@ def create_circuit(n: int):
             qc.cp(angle * np.pi, psi, q[i])
 
     qc.compose(QFT(num_qubits=n, inverse=True), inplace=True, qubits=list(range(n)))
-    qc.measure_all()
+    qc.measure(q, c)
 
     return qc
