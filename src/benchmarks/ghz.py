78d2cbe6652b076fecd8a3c4e635bbe5df866f3f
Nils <nils.quetschlich@gmail.com>
2021-12-07 08:11:53 +0100

    modified and bugfixed some benchmark circuits

--- src/benchmarks/ghz.py
+++ src/benchmarks/ghz.py
@@ -1,9 +1,10 @@
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 
+# checked
+
 def create_circuit(n: int):
     q = QuantumRegister(n, 'q')
-    c = ClassicalRegister(n, 'c')
-    qc = QuantumCircuit(q, c, name='ghz')
+    qc = QuantumCircuit(q, name='ghz')
     qc.h(q[-1])
     for i in range(1, n):
         qc.cx(q[n - i], q[n - i - 1])
