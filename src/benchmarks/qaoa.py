9efd14f08fc4d3ec840bb2701baa1af33500484d
Nils <nils.quetschlich@gmail.com>
2021-12-08 10:38:22 +0100

    checked qaoa and vqe and fixed small bug

--- src/benchmarks/qaoa.py
+++ src/benchmarks/qaoa.py
@@ -1,25 +1,26 @@
-# Code from qiskit-optimization demo
+## Code from qiskit-optimization demo
+
+# checked
 
 from qiskit import Aer
 from qiskit.utils import QuantumInstance
 from qiskit_optimization.algorithms import MinimumEigenOptimizer
 from qiskit.algorithms import QAOA
+from qiskit.algorithms.optimizers import SLSQP
 
 from src.utils import get_examplary_max_cut_qp
 
 
 def create_circuit(n: int):
     qp = get_examplary_max_cut_qp(n)
-    qins = QuantumInstance(backend=Aer.get_backend('qasm_simulator'), shots=1024, seed_simulator=123)
+    sim = QuantumInstance(backend=Aer.get_backend('qasm_simulator'), shots=1024, seed_simulator=123)
 
-    # Define QAOA solver
-    qaoa = QAOA(reps=1, quantum_instance=qins)
-    meo = MinimumEigenOptimizer(min_eigen_solver=qaoa)
-    qaoa_result = meo.solve(qp)
-
-
+    qaoa = QAOA(reps=2, optimizer=SLSQP(), quantum_instance=sim)
+    qaoa_optimizer = MinimumEigenOptimizer(min_eigen_solver=qaoa)
+    qaoa_result = qaoa_optimizer.solve(qp)
     qc = qaoa.get_optimal_circuit()
-    qc.name="QAOA"
+
     qc.measure_all()
+    qc.name="QAOA"
 
     return qc
--- src/benchmarks/vqe.py
+++ src/benchmarks/vqe.py
@@ -1,4 +1,6 @@
-## Code from http://localhost:8888/notebooks/Downloads/qiskit-application-modules-demo-sessions/qiskit-finance/Introduction%20to%20Qiskit%20Finance.ipynb
+## Code from qiskit finance demo
+
+## checked
 
 from src.utils import get_examplary_max_cut_qp
 from qiskit.algorithms import VQE
@@ -6,15 +8,14 @@ from qiskit.algorithms.optimizers import SLSQP
 from qiskit_optimization.algorithms import MinimumEigenOptimizer
 from qiskit.circuit.library import RealAmplitudes
 from qiskit import Aer
+from qiskit.utils import QuantumInstance
 
 
 def create_circuit(n: int):
     qp = get_examplary_max_cut_qp(n)
+    sim = QuantumInstance(backend=Aer.get_backend('qasm_simulator'), shots=1024, seed_simulator=123)
 
-    sim = Aer.get_backend("qasm_simulator")
-    ansatz = RealAmplitudes(3, reps=2)
-
-
+    ansatz = RealAmplitudes(n, reps=2)
     vqe = VQE(ansatz, optimizer=SLSQP(), quantum_instance=sim)
     vqe_optimizer = MinimumEigenOptimizer(vqe)
     vqe_result = vqe_optimizer.solve(qp)
@@ -24,34 +25,3 @@ def create_circuit(n: int):
     qc.name="VQE"
 
     return qc
-
-
-
-
-
-## Code from https://qiskit.org/textbook/ch-applications/vqe-molecules.html
-
-
-# molecule = Molecule(geometry=[['H', [0., 0., 0.]],
-#                               ['H', [0., 0., 0.735]]],
-#                      charge=0, multiplicity=1)
-# driver = ElectronicStructureMoleculeDriver(molecule, basis='sto3g', driver_type=ElectronicStructureDriverType.PYSCF)
-#
-# es_problem = ElectronicStructureProblem(driver)
-# qubit_converter = QubitConverter(JordanWignerMapper())
-#
-# from qiskit.providers.aer import StatevectorSimulator
-# from qiskit import Aer
-# from qiskit.utils import QuantumInstance
-# from qiskit_nature.algorithms import VQEUCCFactory
-#
-# quantum_instance = QuantumInstance(backend = Aer.get_backend('aer_simulator_statevector'))
-# vqe_solver = VQEUCCFactory(quantum_instance)
-#
-# from qiskit_nature.algorithms import GroundStateEigensolver
-#
-# calc = GroundStateEigensolver(qubit_converter, vqe_solver)
-# res = calc.solve(es_problem)
-#
-#
-# print(res)
