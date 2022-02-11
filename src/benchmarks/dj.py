b32f8341a232606ab4d872c4a73bf74ac192faf5
Nils Quetschlich <nils.quetschlich@tum.de>
2022-02-11 15:55:31 +0100

    removed unnecessary comments

--- src/benchmarks/dj.py
+++ src/benchmarks/dj.py
@@ -8,39 +8,25 @@ def dj_oracle(case, n):
     # plus one output qubit
     oracle_qc = QuantumCircuit(n + 1)
 
-    # First, let's deal with the case in which oracle is balanced
     if case == "balanced":
-        # First generate a random number that tells us which CNOTs to
-        # wrap in X-gates:
-        #rng = np.random.default_rng(10)
         np.random.seed = 10
         b_str=""
         for _ in range(n):
             b = np.random.randint(0, 2)
             b_str = b_str + str(b)
-        #b = rng.integers(low=0, high=2)
-        #b = np.random.randint(1, 2 ** n, dtype=np.int64())
-        # Next, format 'b' as a binary string of length 'n', padded with zeros:
-        #b_str = format(b, '0' + str(n) + 'b')
-        # Next, we place the first X-gates. Each digit in our binary string
-        # corresponds to a qubit, if the digit is 0, we do nothing, if it's 1
-        # we apply an X-gate to that qubit:
-        for qubit in range(len(b_str)):
-            if b_str[qubit] == '1':
-                oracle_qc.x(qubit)
-        # Do the controlled-NOT gates for each qubit, using the output qubit
-        # as the target:
-        for qubit in range(n):
-            oracle_qc.cx(qubit, n)
-        # Next, place the final X-gates
+
         for qubit in range(len(b_str)):
             if b_str[qubit] == '1':
                 oracle_qc.x(qubit)
 
-    # Case in which oracle is constant
+        for qubit in range(n):
+            oracle_qc.cx(qubit, n)
+
+        for qubit in range(len(b_str)):
+            if b_str[qubit] == '1':
+                oracle_qc.x(qubit)
+
     if case == "constant":
-        # First decide what the fixed output of the oracle will be
-        # (either always 0 or always 1)
         output = np.random.randint(2)
         if output == 1:
             oracle_qc.x(n)
@@ -52,15 +38,15 @@ def dj_oracle(case, n):
 
 def dj_algorithm(oracle, n):
     dj_circuit = QuantumCircuit(n + 1, n)
-    # Set up the output qubit:
+
     dj_circuit.x(n)
     dj_circuit.h(n)
-    # And set up the input register:
+
     for qubit in range(n):
         dj_circuit.h(qubit)
-    # Let's append the oracle gate to our circuit:
+
     dj_circuit.append(oracle, range(n + 1))
-    # Finally, perform the H-gates again and measure:
+
     for qubit in range(n):
         dj_circuit.h(qubit)
 
