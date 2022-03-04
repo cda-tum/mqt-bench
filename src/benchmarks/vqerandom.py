1d6dde5b01f528fdeede9f4f6fcca919fb9ec125
Nils Quetschlich <Nils.Quetschlich@tum.de>
2022-03-04 10:29:43 +0100

    🐛 adjusted seed

--- src/benchmarks/vqerandom.py
+++ src/benchmarks/vqerandom.py
@@ -10,7 +10,7 @@ def create_circuit(num_qubits: int):
     num_qubits -- number of qubits of the returned quantum circuit
     """
 
-    np.random.seed(1234)
+    np.random.seed = 10
     qc = RealAmplitudes(num_qubits, reps=2)
     num_params = qc.num_parameters
     qc = qc.bind_parameters(np.random.rand(num_params))
