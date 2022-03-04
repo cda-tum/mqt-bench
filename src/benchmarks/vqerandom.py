3ca404fcdac48c7d6ec42824240c18f0511ba149
Nils Quetschlich <Nils.Quetschlich@tum.de>
2022-03-04 10:08:51 +0100

    ✨ added VQE random benchmark where the parameter values will not be optimized

---
+++ src/benchmarks/vqerandom.py
@@ -0,0 +1,20 @@
+from qiskit.circuit.library import RealAmplitudes
+import numpy as np
+
+
+def create_circuit(num_qubits: int):
+    """Returns a quantum circuit implementing the Variational Quantum Eigensolver Algorithm with random parameter
+    values.
+
+    Keyword arguments:
+    num_qubits -- number of qubits of the returned quantum circuit
+    """
+
+    np.random.seed(1234)
+    qc = RealAmplitudes(num_qubits, reps=2)
+    num_params = qc.num_parameters
+    qc = qc.bind_parameters(np.random.rand(num_params))
+    qc.measure_all()
+    qc.name = "vqerandom"
+
+    return qc
