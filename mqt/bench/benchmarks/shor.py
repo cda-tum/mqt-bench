39e92746336c14d17d1ba6a109d8b58f56cdef64
Nils Quetschlich <Nils.Quetschlich@tum.de>
2022-04-05 08:48:31 +0200

    ✨ added barrier and measurements

--- mqt/bench/benchmarks/shor.py
+++ mqt/bench/benchmarks/shor.py
@@ -9,7 +9,8 @@ def create_circuit(num_to_be_factorized: int, a: int = 2):
     a -- any integer that satisfies 1 < a < num_to_be_factorized and gcd(a, num_to_be_factorized) = 1
     """
 
-    qc = Shor().construct_circuit(num_to_be_factorized, a, measurement=True)
+    qc = Shor().construct_circuit(num_to_be_factorized, a)
+    qc.measure_all()
     qc.name = "shor_" + str(num_to_be_factorized) + "_" + str(a)
 
     return qc
