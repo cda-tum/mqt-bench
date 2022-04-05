86f046f046d9c0c4bf7bf014497523d3e7ce00e5
Nils Quetschlich <Nils.Quetschlich@tum.de>
2022-04-05 18:21:59 +0200

    ✨ adjusted setup.py to differentiate between required and extra dependencies

--- setup.py
+++ setup.py
@@ -27,19 +27,22 @@ setup(
     download_url="https://github.com/user/reponame/archive/v_01.tar.gz",
     keywords="mqt quantum benchmarking performance testing",
     install_requires=[
-        "numpy~=1.22.3",
         "matplotlib~=3.5.1",
         "qiskit~=0.34.1",
-        "qiskit-finance~=0.3.1",
-        "qiskit-machine-learning==0.3.1",
-        "qiskit-nature[pyscf]~=0.3.1",
-        "qiskit-optimization~=0.3.1",
-        "qiskit-aqua~=0.9.5",
         "pandas~=1.3.5",
         "flask~=2.0.2",
         "networkx~=2.7.1",
         "pytest~=7.1.1",
     ],
+    extras_require={
+        "all": [
+            "qiskit-finance~=0.3.1",
+            "qiskit-machine-learning==0.3.1",
+            "qiskit-nature~=0.3.1",
+            "qiskit-optimization~=0.3.2",
+            "qiskit-aqua~=0.9.5",
+        ],
+    },
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
