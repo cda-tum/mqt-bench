9079c35cd89b415ea23e52b4c8756f2745d02593
Nils Quetschlich <Nils.Quetschlich@tum.de>
2022-06-07 16:06:41 +0200

    📌 updated substituted depency as suggested in PR comment

--- setup.py
+++ setup.py
@@ -27,13 +27,12 @@ setup(
     url="https://github.com/cda-tum/mqtbench",
     keywords="mqt quantum benchmarking performance testing",
     install_requires=[
-        "matplotlib==3.5.1",
         "qiskit==0.35",
+        "qiskit[visualization]==0.35.0",
         "pandas==1.3.5",
         "flask==2.1.2",
         "networkx==2.8.3",
         "pytest==7.1.1",
-        "pylatexenc~=2.10",
     ],
     extras_require={
         "all": [
