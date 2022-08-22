071db973b7db8acc929137fa86185d41826509ea
Nils Quetschlich <Nils.Quetschlich@tum.de>
2022-08-22 17:13:44 +0200

    🔥 removed wsgi.py file cause the server is now started differently

--- mqt/benchviewer/wsgi.py
+++
@@ -1,6 +0,0 @@
-from main import app
-
-if __name__ == "__main__":
-    #    app.config["SERVER_NAME"] = "cda.cit.tum.de"
-    #    app.config["APPLICATION_ROOT"] = "/app/benchviewer/"
-    app.run()
