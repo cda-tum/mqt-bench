e8e56bfe7a6c43db24d5324bba7f3541be463f63
Nils Quetschlich <Nils.Quetschlich@tum.de>
2022-08-22 13:49:01 +0200

    📌 added dependency for progressbar

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         "qiskit-nature[pyscf]~=0.3.1",
         "qiskit_optimization~=0.3.2",
         "packaging~=21.3",
+        "tqdm~=4.64.0",
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
