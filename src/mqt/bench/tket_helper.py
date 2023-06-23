ebc4c5e8ba98a6cb30e9c9f10954f5af55fb102a
Nils Quetschlich <Nils.Quetschlich@tum.de>
2023-06-23 19:08:26 +0200

    🎨 simplified TKET mapped compilation based on new pytket v1.16 release

--- src/mqt/bench/tket_helper.py
+++ src/mqt/bench/tket_helper.py
@@ -8,10 +8,9 @@ from pytket import OpType
 from pytket.architecture import Architecture  # type: ignore[attr-defined]
 from pytket.extensions.qiskit import qiskit_to_tk
 from pytket.passes import (  # type: ignore[attr-defined]
-    CliffordSimp,
     CXMappingPass,
     FullPeepholeOptimise,
-    KAKDecomposition,
+    PeepholeOptimise2Q,
     PlacementPass,
     RoutingPass,
     SynthesiseTket,
@@ -323,9 +322,7 @@ def get_mapped_level(
     placer = LinePlacement(arch) if lineplacement else GraphPlacement(arch)
     PlacementPass(placer).apply(qc_tket)
     RoutingPass(arch).apply(qc_tket)
-    SynthesiseTket().apply(qc_tket)
-    KAKDecomposition(allow_swaps=False).apply(qc_tket)
-    CliffordSimp(allow_swaps=False).apply(qc_tket)
+    PeepholeOptimise2Q(allow_swaps=False).apply(qc_tket)
     SynthesiseTket().apply(qc_tket)
     if not qc_tket.valid_connectivity(arch, directed=True):
         CXMappingPass(arc=arch, placer=placer, directed_cx=True, delay_measures=False).apply(qc_tket)
