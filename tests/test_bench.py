3f574c6291cc13b0f45dc55a47296adda1ed45bb
Nils Quetschlich <Nils.Quetschlich@tum.de>
2023-09-05 15:53:12 +0200

    🎨 improved method signatures

--- tests/test_bench.py
+++ tests/test_bench.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import json
 from pathlib import Path
-from typing import TYPE_CHECKING, Literal
+from typing import TYPE_CHECKING
 
 import pytket
 
@@ -655,7 +655,7 @@ def test_get_benchmark(
     level: str | int,
     circuit_size: int,
     benchmark_instance_name: str | None,
-    compiler: Literal["qiskit", "tket"],
+    compiler: str,
     compiler_settings: CompilerSettings | None,
     gate_set_name: str,
     device_name: str,
@@ -724,7 +724,7 @@ def test_get_benchmark_faulty_parameters() -> None:
 
     match = "Selected compiler must be in"
     with pytest.raises(ValueError, match=match):
-        get_benchmark(  # type: ignore[call-overload]
+        get_benchmark(
             "qpeexact",
             1,
             3,
