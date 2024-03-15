3d0e36369ee60cfdfae24565d51a1f7dbda13577
Nils Quetschlich <Nils.Quetschlich@tum.de>
2024-03-15 09:43:40 +0100

    ✨ adjusted qiskit version str handling

--- src/mqt/bench/utils.py
+++ src/mqt/bench/utils.py
@@ -14,7 +14,8 @@ from importlib import import_module
 import networkx as nx
 import numpy as np
 from pytket import __version__ as __tket_version__
-from qiskit import QuantumCircuit, __version__
+from qiskit import QuantumCircuit
+from qiskit import __version__ as __qiskit_version__
 from qiskit.converters import circuit_to_dag
 from qiskit_optimization.applications import Maxcut
 
@@ -195,7 +196,7 @@ def save_as_qasm(
         f.write("// For more information about MQT Bench, please visit https://www.cda.cit.tum.de/mqtbench/\n")
         f.write("// MQT Bench version: " + mqtbench_module_version + "\n")
         if "qiskit" in filename:
-            f.write("// Qiskit version: " + str(__version__) + "\n")
+            f.write("// Qiskit version: " + str(__qiskit_version__) + "\n")
         elif "tket" in filename:
             f.write("// TKET version: " + str(__tket_version__) + "\n")
         if gate_set:
