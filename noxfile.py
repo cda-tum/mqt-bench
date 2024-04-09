310efa7b2f7b81193a587503ad2d26bc090babfc
burgholzer <burgholzer@me.com>
2024-04-09 17:11:09 +0200

    ✨ add noxfile for easy script running
    Signed-off-by: burgholzer <burgholzer@me.com>

---
+++ noxfile.py
@@ -0,0 +1,63 @@
+"""Nox sessions."""
+
+from __future__ import annotations
+
+import argparse
+import os
+
+import nox
+
+nox.needs_version = ">=2024.3.2"
+nox.options.default_venv_backend = "uv|virtualenv"
+
+PYTHON_ALL_VERSIONS = ["3.10", "3.11", "3.12"]
+
+BUILD_REQUIREMENTS = [
+    "setuptools>=61",
+    "setuptools_scm>=7",
+]
+
+if os.environ.get("CI", None):
+    nox.options.error_on_missing_interpreters = True
+
+
+@nox.session(reuse_venv=True)
+def lint(session: nox.Session) -> None:
+    """Lint the Python part of the codebase using pre-commit.
+
+    Simply execute `nox -rs lint` to run all configured hooks.
+    """
+    session.install("pre-commit")
+    session.run("pre-commit", "run", "--all-files", *session.posargs)
+
+
+@nox.session(reuse_venv=True)
+def docs(session: nox.Session) -> None:
+    """Build the docs. Use "--non-interactive" to avoid serving. Pass "-b linkcheck" to check links."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", dest="builder", default="html", help="Build target (default: html)")
+    args, posargs = parser.parse_known_args(session.posargs)
+
+    serve = args.builder == "html" and session.interactive
+    extra_installs = ["sphinx-autobuild"] if serve else []
+    session.install(*BUILD_REQUIREMENTS, *extra_installs)
+    session.install("--no-build-isolation", "-ve.[docs]")
+    session.chdir("docs")
+
+    if args.builder == "linkcheck":
+        session.run("sphinx-build", "-b", "linkcheck", ".", "_build/linkcheck", *posargs)
+        return
+
+    shared_args = (
+        "-n",  # nitpicky mode
+        "-T",  # full tracebacks
+        f"-b={args.builder}",
+        ".",
+        f"_build/{args.builder}",
+        *posargs,
+    )
+
+    if serve:
+        session.run("sphinx-autobuild", *shared_args)
+    else:
+        session.run("sphinx-build", "--keep-going", *shared_args)
