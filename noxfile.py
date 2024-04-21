58f397bacbb4906b558ef4a48486e61b44c17186
Nils Quetschlich <Nils.Quetschlich@tum.de>
2024-04-21 16:12:07 +0200

    👷 improved nox file

--- noxfile.py
+++ noxfile.py
@@ -4,11 +4,17 @@ from __future__ import annotations
 
 import argparse
 import os
+from typing import TYPE_CHECKING
 
 import nox
 
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+
 nox.needs_version = ">=2024.3.2"
 nox.options.default_venv_backend = "uv|virtualenv"
+
 
 PYTHON_ALL_VERSIONS = ["3.10", "3.11", "3.12"]
 
@@ -29,6 +35,45 @@ def lint(session: nox.Session) -> None:
     """
     session.install("pre-commit")
     session.run("pre-commit", "run", "--all-files", *session.posargs)
+
+
+def _run_tests(
+    session: nox.Session,
+    *,
+    install_args: Sequence[str] = (),
+    run_args: Sequence[str] = (),
+    extras: Sequence[str] = (),
+) -> None:
+    posargs = list(session.posargs)
+    env = {"PIP_DISABLE_PIP_VERSION_CHECK": "1"}
+
+    _extras = ["test", *extras]
+    if "--cov" in posargs:
+        _extras.append("coverage")
+        posargs.append("--cov-config=pyproject.toml")
+
+    session.install(*BUILD_REQUIREMENTS, *install_args, env=env)
+    install_arg = f"-ve.[{','.join(_extras)}]"
+    session.install("--no-build-isolation", install_arg, *install_args, env=env)
+    session.run("pytest", *run_args, *posargs, env=env)
+
+
+@nox.session(reuse_venv=True, python=PYTHON_ALL_VERSIONS)
+def tests(session: nox.Session) -> None:
+    """Run the test suite."""
+    _run_tests(session)
+
+
+@nox.session(reuse_venv=True, venv_backend="uv")
+def minimums(session: nox.Session) -> None:
+    """Test the minimum versions of dependencies."""
+    _run_tests(
+        session,
+        install_args=["--resolution=lowest-direct"],
+        run_args=["-Wdefault"],
+        extras=["qiskit", "evaluation"],
+    )
+    session.run("uv", "pip", "list")
 
 
 @nox.session(reuse_venv=True)
@@ -58,6 +103,6 @@ def docs(session: nox.Session) -> None:
     )
 
     if serve:
-        session.run("sphinx-autobuild", *shared_args)
+        session.run("sphinx-autobuild", "--ignore", "**jupyter**", *shared_args)
     else:
         session.run("sphinx-build", "--keep-going", *shared_args)
