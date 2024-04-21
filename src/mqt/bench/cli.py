f299681f2dd429d8a8a9813050bef337c5464b94
ColoredCarrot <7807570+ColoredCarrot@users.noreply.github.com>
2024-04-21 17:42:54 +0200

    Add basic CLI around get_benchmark() (#321)
    Mainly to be used automatically by tooling.
    Options are simple CLI options for now, but should probably become more
    dynamic in the future (does argparse support dynamic/unregistered
    options?).
    
    ---------
    
    Signed-off-by: burgholzer <burgholzer@me.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Lukas Burgholzer <burgholzer@me.com>

---
+++ src/mqt/bench/cli.py
@@ -0,0 +1,83 @@
+from __future__ import annotations
+
+import argparse
+from typing import cast
+
+from pytket.qasm import circuit_to_qasm_str
+from qiskit import QuantumCircuit
+from qiskit.qasm2 import dumps as qiskit_circuit_to_str
+
+from mqt.bench import CompilerSettings, QiskitSettings, TKETSettings, get_benchmark
+
+
+def main() -> None:
+    """
+    Invoke :func:`get_benchmark` exactly once with the specified arguments.
+
+    The resulting QASM string is printed to the standard output stream;
+    no other output is generated.
+    """
+
+    parser = argparse.ArgumentParser(description="Generate a single benchmark")
+    parser.add_argument(
+        "--level",
+        type=str,
+        help='Level to generate benchmarks for ("alg", "indep", "nativegates" or "mapped")',
+        required=True,
+    )
+    parser.add_argument("--algorithm", type=str, help="Name of the benchmark", required=True)
+    parser.add_argument("--num-qubits", type=int, help="Number of Qubits", required=True)
+    parser.add_argument("--compiler", type=str, help="Name of the compiler")
+    parser.add_argument("--qiskit-optimization-level", type=int, help="Qiskit compiler optimization level")
+    parser.add_argument("--tket-placement", type=str, help="TKET placement")
+    parser.add_argument("--native-gate-set", type=str, help="Name of the provider")
+    parser.add_argument("--device", type=str, help="Name of the device")
+    args = parser.parse_args()
+
+    qiskit_settings = QiskitSettings()
+    if args.qiskit_optimization_level is not None:
+        qiskit_settings = QiskitSettings(args.qiskit_optimization_level)
+
+    tket_settings = TKETSettings()
+    if args.tket_placement is not None:
+        tket_settings = TKETSettings(args.tket_placement)
+
+    # Note: Assertions about argument validity are in get_benchmark()
+
+    # Temporary workaround to "get things working" with benchmark instances.
+    # This special treatment should be removed as soon as instance handling has been refactored in get_benchmark().
+    benchmark_name, benchmark_instance = parse_benchmark_name_and_instance(args.algorithm)
+
+    result = get_benchmark(
+        benchmark_name=benchmark_name,
+        benchmark_instance_name=benchmark_instance,
+        level=args.level,
+        circuit_size=args.num_qubits,
+        compiler=args.compiler,
+        compiler_settings=CompilerSettings(
+            qiskit=qiskit_settings,
+            tket=tket_settings,
+        ),
+        provider_name=args.native_gate_set,
+        device_name=args.device,
+    )
+
+    if isinstance(result, QuantumCircuit):
+        print(qiskit_circuit_to_str(result))
+        return
+
+    print(circuit_to_qasm_str(result))
+
+
+def parse_benchmark_name_and_instance(algorithm: str) -> tuple[str, str | None]:
+    """
+    Parse an algorithm name like "shor_xlarge" into a benchmark and instance name
+    as expected by :func:`get_benchmark`.
+    """
+
+    if algorithm.startswith(("shor_", "groundstate_")):
+        as_list = algorithm.split("_", 2)
+        assert len(as_list) == 2
+        return cast(tuple[str, str], tuple(as_list))
+
+    return algorithm, None
---
+++ tests/test_cli.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+from qiskit.qasm2 import dumps
+
+from mqt.bench import CompilerSettings, QiskitSettings, get_benchmark
+
+if TYPE_CHECKING:
+    from pytest_console_scripts import ScriptRunner
+
+
+# fmt: off
+@pytest.mark.parametrize(
+    ("args", "expected_output"),
+    [
+        ([
+             "--level", "alg",
+             "--algorithm", "ghz",
+             "--num-qubits", "10",
+         ], dumps(get_benchmark(level="alg", benchmark_name="ghz", circuit_size=10))),
+        ([
+             "--level", "alg",
+             "--algorithm", "shor_xsmall",
+             "--num-qubits", "10",
+         ], "OPENQASM 2.0;"),  # Note: shor is non-deterministic, so just a basic sanity check
+        ([
+             "--level", "alg",
+             "--algorithm", "ghz",
+             "--num-qubits", "20",
+         ], dumps(get_benchmark(level="alg", benchmark_name="ghz", circuit_size=20))),
+        ([
+             "--level", "indep",
+             "--algorithm", "ghz",
+             "--num-qubits", "20",
+             "--compiler", "qiskit",
+         ], dumps(get_benchmark(level="indep", benchmark_name="ghz", circuit_size=20, compiler="qiskit"))),
+        ([
+             "--level", "mapped",
+             "--algorithm", "ghz",
+             "--num-qubits", "20",
+             "--compiler", "qiskit",
+             "--qiskit-optimization-level", "2",
+             "--native-gate-set", "ibm",
+             "--device", "ibm_montreal",
+         ], dumps(get_benchmark(
+            level="mapped",
+            benchmark_name="ghz",
+            circuit_size=20,
+            compiler="qiskit",
+            compiler_settings=CompilerSettings(QiskitSettings(optimization_level=2)),
+            provider_name="ibm",
+            device_name="ibm_montreal",
+        ))),
+    ],
+)
+def test_cli(args: list[str], expected_output: str, script_runner: ScriptRunner) -> None:
+    ret = script_runner.run(["mqt.bench.cli", *args])
+    assert ret.success
+    assert expected_output in ret.stdout
+
+
+# fmt: off
+@pytest.mark.parametrize(
+    ("args", "expected_output"),
+    [
+        ([], "usage: mqt.bench.cli"),
+        (["asd"], "usage: mqt.bench.cli"),
+        (["--benchmark", "ae"], "usage: mqt.bench.cli"),
+        # Note: We don't care about the actual error messages in most cases
+        ([
+             "--level", "indep",
+             "--algorithm", "ghz",
+             "--num-qubits", "20",
+             # Missing compiler option
+         ], ""),
+        ([
+             "--level", "alg",
+             "--algorithm", "not-a-valid-benchmark",
+             "--num-qubits", "20",
+         ], ""),
+    ],
+)
+def test_cli_errors(args: list[str], expected_output: str, script_runner: ScriptRunner) -> None:
+    ret = script_runner.run(["mqt.bench.cli", *args])
+    assert not ret.success
+    assert expected_output in ret.stderr
