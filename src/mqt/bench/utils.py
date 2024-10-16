510be9daac594eff11c3efef67d14dab665ff82c
burgholzer <burgholzer@me.com>
2024-10-17 00:37:36 +0200

    🔥 eliminate `convert_cmap_to_tuple_list`
    Signed-off-by: burgholzer <burgholzer@me.com>

--- src/mqt/bench/utils.py
+++ src/mqt/bench/utils.py
@@ -258,8 +258,3 @@ def calc_supermarq_features(
 def get_module_for_benchmark(benchmark_name: str) -> ModuleType:
     """Returns the module for a specific benchmark."""
     return import_module("mqt.bench.benchmarks." + benchmark_name)
-
-
-def convert_cmap_to_tuple_list(c_map: list[list[int]]) -> list[tuple[int, int]]:
-    """Converts a coupling map to a list of tuples."""
-    return [(c[0], c[1]) for c in c_map]
