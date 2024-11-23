b3dafff7fe6c6c73e7326b2633c706d0fa8492f4
Nils Quetschlich <nils.quetschlich@tum.de>
2024-11-23 04:47:45 -0500

    ✅ increase coverage

--- tests/test_clifford+t.py
+++ tests/test_clifford+t.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
+import pytest
+
 from mqt.bench import get_benchmark
 from mqt.bench.devices import get_native_gateset_by_name
 
@@ -18,3 +20,14 @@ def test_clifford_t() -> None:
 
     for gate_type in qc.count_ops():
         assert gate_type in get_native_gateset_by_name("clifford+t").gates or gate_type in {"measure", "barrier"}
+
+    with pytest.raises(
+        ValueError, match=r"The gateset 'clifford\+t' is not supported by TKET. Please use Qiskit instead."
+    ):
+        get_benchmark(
+            benchmark_name="ghz",
+            level="nativegates",
+            circuit_size=3,
+            compiler="tket",
+            gateset="clifford+t",
+        )
