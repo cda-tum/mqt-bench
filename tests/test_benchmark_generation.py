6ea6582507895d49f618890e64d9972541a2bdda
Nils Quetschlich <nils.quetschlich@tum.de>
2024-11-26 17:18:04 +0100

    ✅ removed unnecessary assert statement

--- tests/test_benchmark_generation.py
+++ tests/test_benchmark_generation.py
@@ -368,10 +368,9 @@ def evaluate_benchmark_with_qasm_formats(
     """Evaluate the benchmarks with different QASM formats."""
     for qasm_format in ["qasm2", "qasm3"]:
         res = fct(*args, qasm_format=qasm_format)
+        assert res
         filepath = Path(output_path) / "testfile.qasm"
         if qasm_format == "qasm2":
             assert load_qasm2(filepath, custom_instructions=LEGACY_CUSTOM_INSTRUCTIONS)
-            assert res
         else:
             assert load_qasm3(filepath)
-            assert res
