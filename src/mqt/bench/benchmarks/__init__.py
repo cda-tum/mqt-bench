b1a93ba007c37e590f4a28429aa0b5cf5ea80532
burgholzer <burgholzer@me.com>
2024-12-02 17:59:00 +0100

    🚚 no special handling for QNN
    Signed-off-by: burgholzer <burgholzer@me.com>

--- src/mqt/bench/benchmarks/__init__.py
+++ src/mqt/bench/benchmarks/__init__.py
@@ -5,9 +5,3 @@ See README.md or go to https://github.com/cda-tum/mqt-bench for more information
 """
 
 from __future__ import annotations
-
-from mqt.bench.benchmarks.qiskit_application_ml import qnn
-
-__all__ = [
-    "qnn",
-]
