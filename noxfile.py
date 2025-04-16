ddcda7e799d68474f04b1419e8d2cad0c046915f
Nils Quetschlich <nils.quetschlich@tum.de>
2025-04-16 16:33:42 +0200

    updated config and lock file

--- noxfile.py
+++ noxfile.py
@@ -100,8 +100,6 @@ def docs(session: nox.Session) -> None:
         "uv",
         "run",
         "--no-dev",
-        "--group",
-        "docs",
         "--frozen",
         "sphinx-autobuild" if serve else "sphinx-build",
         *shared_args,
